b5bfefccfeac7260b1f93b63363aade87dd5fcaa
Pablo Sanchez <pablo.gon.sanchez@hpe.com>
2019-04-26 12:57:00 +0100

    configuration URL

--- inspectorConfig/inspector_config.go
+++ inspectorConfig/inspector_config.go
@@ -7,5 +7,5 @@ var (
 
 // LoadConfig returns a map of the config
 func LoadConfig() {
-	InspectorConfiguration = InspectorConfiguration.unmarshallYaml("https://raw.github.hpe.com/pablo-gon-sanchez/inspector-gadget/master/config/config.yml")
+	InspectorConfiguration = InspectorConfiguration.unmarshallYaml("https://raw.github.hpe.com/pablo-gon-sanchez/inspector-gadget/master/inspectorConfig/config.yml")
 }
